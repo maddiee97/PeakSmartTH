b740f57ca259062012cee2b8280d7fe9f3fe1b76
maddiee97 <madison.maganda97@gmail.com>
2025-06-07 01:37:46 +0700

    upd

--- backend/src/controllers/loginController.js
+++ backend/src/controllers/loginController.js
@@ -12,10 +12,10 @@ const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-    const token = jwt.sign(
-      { id: user._id },
-      process.env.JWT_SECRET, // make sure this is defined!
-      { expiresIn: '1d' }
+     const token = jwt.sign(
+    { id: user._id, username: user.username, email: user.email, provider: user.provider },
+    process.env.JWT_SECRET, // ✅ this must be set
+    { expiresIn: '1d' }
     );
 
     res.status(200).json({
